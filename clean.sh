4e29c97222120c51e4eb8d9e4cb7f300b4b62209
leone <791960492@qq.com>
2023-06-22 10:21:08 +0800

    update

--- clean.sh
+++ clean.sh
@@ -8,4 +8,4 @@ then
 fi
 make -C ${CUR_DIR}/Linux clean -s
 make -C ${CUR_DIR}/Linux clean -s DEBUG=1
-rm -rf ${CUR_DIR}/Linux/package/lib64/* ${CUR_DIR}/Linux/package/include/crypto/
+rm -rf ${CUR_DIR}/Linux/package/lib64/* ${CUR_DIR}/Linux/package/include/crypto/ ${CUR_DIR}/Linux/sgx/test_app/InstrumentStatistics.json
